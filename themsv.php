cbe3b9a0d0c7456f8b749f99b70ba444b4d5a09e
DAO THU TRANG <71755819+daothutrang@users.noreply.github.com>
2023-04-10 14:54:23 +0700

    update

--- themsv.php
+++ themsv.php
@@ -1,50 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
-<body>
-<h1>Thêm sinh viên</h1>
-	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-		<label for="mssv">Mssv:</label><br>
-		<input type="text" id="mssv" name="mssv"><br>
-		<label for="hoten">Họ tên:</label><br>
-		<input type="text" id="hoten" name="hoten"><br>
-		<label for="namsinh">Năm sinh:</label><br>
-		<input type="text" id="namsinh" name="namsinh"><br>
-		<label for="sdt">SDT:</label><br>
-		<input type="text" id="sdt" name="sdt"><br>
-		<input type="submit" name="submit" value="Thêm">
-	</form>
+    <head>
+        <title>Thêm sinh viên</title>
+    </head>
+    <body>
+        <h3> Thêm sinh viên </h3>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" class="border">
+            Mã sinh viên<BR> <input type="text" name="masv" value ="<?php echo $masv;?>"><BR>
+            Họ và tên<BR> <input type="text" name="name" value ="<?php echo $name;?>"><BR>
+            Năm sinh<BR> <input type="text" name="birthday" value ="<?php echo $birthday ?>"><BR>
+            Điện thoại<BR> <input type="text" name="phone" value ="<?php echo $phone ?>"><BR>
+            <input type="submit" name="submit" value="submit">
+        </form> 
 
+        <?php
+            if($_SERVER["REQUEST_METHOD"] == "POST") {
+                $masv = $_POST["masv"];
+                $name = $_POST["name"];
+                $birthday = $_POST["birthday"];
+                $phone = $_POST["phone"];
+    
+                $MYSQL_ADDON_HOST = getenv('MYSQL_ADDON_HOST');
+                $MYSQL_ADDON_PORT = getenv('MYSQL_ADDON_PORT');
+                $MYSQL_ADDON_DB = getenv('MYSQL_ADDON_DB');
+                $MYSQL_ADDON_USER = getenv('MYSQL_ADDON_USER');
+                $MYSQL_ADDON_PASSWORD = getenv('MYSQL_ADDON_PASSWORD');
+    
+                $conn = mysqli_connect($MYSQL_ADDON_HOST, $MYSQL_ADDON_USER, $MYSQL_ADDON_PASSWORD, $MYSQL_ADDON_DB, $MYSQL_ADDON_PORT);
+    
+                if (!$conn) {
+                    echo "<br>" . "Error: Không thể kết nối với cơ sở dữ liệu.";
+                } else {
+                    echo "<br>" . "Đã kết nối với CSDL." . "<br>";
+                }
+    
+                $sql = "INSERT INTO b1910005_sinhvien (masv, hoten, namsinh, dienthoai) VALUES ('$masv', '$name', '$birthday', '$phone')";
+    
+                if (mysqli_query($conn, $sql)) {
+                    echo"<br>" . "Thêm sinh viên thành công.";
+                    echo "<script>alert('Đã thêm sinh viên thành công.');</script>";
+                } else {
+                    echo "<br>" . "ERROR: Không thể thêm sinh viên $sql. " . mysqli_error($conn);
+                    echo "<script>alert('Không thể thêm sinh viên.');</script>";
+                }
+    
+                mysqli_close($conn);
+            }
+        ?>
 
-    <?php
-	$mssv = $_POST["mssv"];
-        $hoten = $_POST["hoten"];
-	$namsinh = $_POST["namsinh"];
-	$sdt = $_POST["sdt"];
-
-        $MYSQL_ADDON_HOST = getenv('MYSQL_ADDON_HOST');
-        $MYSQL_ADDON_PORT = getenv('MYSQL_ADDON_PORT');
-        $MYSQL_ADDON_BD = getenv('MYSQL_ADDON_DB');
-        $MYSQL_ADDON_USER = getenv('MYSQL_ADDON_USER');
-        $MYSQL_ADDON_PASSWORD = getenv('MYSQL_ADDON_PASSWORD');
-
-        $conn = mysqli_connect($MYSQL_ADDON_HOST, $MYSQL_ADDON_USER, $MYSQL_ADDON_PASSWORD, $MYSQL_ADDON_BD);
-
-        if (!$conn) {
-            echo "Error: Unable to open database\n";
-        } else {
-            echo "Open database successfully\n";
-        }
-
-        $sql = "INSERT INTO B1910005_paas_db (masv,hoten, nam_sinh, dienthoai) VALUES ('$mssv','$hoten', '$namsinh', '$sdt')";
-        if (mysqli_query($conn, $sql)) {
-            echo "User added successfully.";
-        } else {
-            echo "ERROR: Could not able to execute $sql." . mysqli_error($conn);
-        }
-
-        mysqli_close($conn);
-    ?>
-    <br>
-	<a href="index.php">Trở về trang chủ</a>
-</body>
+        <!-- Thêm thẻ HTML để tạo liên kết trở về trang index.php -->
+        <br> <a href="index.php">Quay lại trang chủ</a> <br>
+    </body>
 </html>
